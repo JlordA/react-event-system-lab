662263e908abc0d78210d06c3a32e66043d5471e
JlordA <james.ardery@gmail.com>
2020-12-09 17:46:44 -0500

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
+import React from 'react'
 // Code EyesOnMe Component Here
+class EyesOnMe extends React.Component {
+
+    focusHandler = () => {
+        console.log('Good!')
+    }
+    
+    blurHandler = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    
+    render() {
+        return <button onFocus={this.focusHandler} onBlur={this.blurHandler} />
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
+import React from 'react'
 // Code Keypad Component Here
+
+class Keypad extends React.Component {
+
+    upHandler = () =>{
+        console.log('Entering password...')
+    }
+    render (){
+        return <input onKeyUp={this.upHandler} type="password" /> 
+    }
+}
+
+export default Keypad
